2a13b228448450bea96c7cb515ee0136f265de21
seyoung8239 <seyoung8239@gmail.com>
2022-07-01 20:40:08 +0900

    Feat: add todoAdd

--- src/components/Todos/TodoAddInput.tsx
+++ src/components/Todos/TodoAddInput.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { useAppDispatch } from '../../hooks';
+import { addTodo } from '../../store/todoSlice';
 
 const TodoAddInput = () => {
+	const dispatch = useAppDispatch();
+
+	const [newTodoContent, setNewTodoContent] = useState<string>('');
+	const handleInputSubmit = useCallback(
+		(event: React.FormEvent) => {
+			if (newTodoContent) {
+				dispatch(addTodo(newTodoContent));
+				setNewTodoContent('');
+				event.preventDefault();
+			}
+		},
+		[dispatch, newTodoContent],
+	);
+
 	return (
 		<>
-			todo input
-			<input type="text" placeholder="let me know what todo" />
+			<form onSubmit={handleInputSubmit}>
+				<input
+					value={newTodoContent}
+					type="text"
+					placeholder="let me know what todo"
+					onChange={e => setNewTodoContent(e.target.value)}
+				/>
+				<button type="submit">Add</button>
+			</form>
 		</>
 	);
 };
